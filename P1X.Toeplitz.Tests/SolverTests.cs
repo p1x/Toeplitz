8cbd7c4baf656105bf5bf7b46cd4458c38f44262
p1x <p1xel.com@gmail.com>
2020-11-26 22:11:22 +0300

    Add Solver class and some tests.

---
+++ P1X.Toeplitz.Tests/SolverTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xunit;
+
+namespace P1X.Toeplitz.Tests {
+    public class SolverTests {
+        [Fact]
+        public void SolveDefaultParameters_ThrowException() {
+            Assert.Throws<ArgumentNullException>(() => Solver.Solve(NormalizedToeplitzMatrix.Create(2), null, new float[2]));
+            Assert.Throws<ArgumentNullException>(() => Solver.Solve(NormalizedToeplitzMatrix.Create(2), new float[2], null));
+            Assert.Throws<ArgumentException>(() => Solver.Solve(default, new float[2], new float[2]));
+        }
+
+        [Fact]
+        public void SolveDifferentVectorAndMatrixSizes_ThrowException() {
+            Assert.Throws<ArgumentException>(() => Solver.Solve(NormalizedToeplitzMatrix.Create(3), new float[2], new float[2]));
+            Assert.Throws<ArgumentException>(() => Solver.Solve(NormalizedToeplitzMatrix.Create(2), new float[3], new float[2]));
+            Assert.Throws<ArgumentException>(() => Solver.Solve(NormalizedToeplitzMatrix.Create(2), new float[2], new float[3]));
+        }
+
+        [Fact]
+        public void Solve_ValidResult() {
+            // Wolfram Alpha query:
+            // ToeplitzMatrix[{1, 2, 3}, {1, 2, 3}] . {x1, x2, x3} == {14, 10, 10}
+            // Result: x1 == 1 && x2 == 2 && x3 == 3
+            var matrix = NormalizedToeplitzMatrix.Create(3);
+            matrix[1] = matrix[-1] = 2;
+            matrix[2] = matrix[-2] = 3;
+            var rightVector = new float[] { 14, 10, 10 };
+            var resultVector = new float[3];
+            
+            Solver.Solve(matrix, rightVector, resultVector);
+            
+            Assert.Equal(1, resultVector[0]);
+            Assert.Equal(2, resultVector[1]);
+            Assert.Equal(3, resultVector[2]);
+        }
+    }
+}
---
+++ P1X.Toeplitz/Solver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P1X.Toeplitz {
+    public static class Solver {
+        public static void Solve(NormalizedToeplitzMatrix matrix, float[] rightVector, float[] resultVector) {
+            if (!matrix.IsInitialized)
+                throw new ArgumentException("The matrix should be initialized (non-default).", nameof(matrix));
+            if (rightVector == null)
+                throw new ArgumentNullException(nameof(rightVector));
+            if (resultVector == null)
+                throw new ArgumentNullException(nameof(resultVector));
+
+            if (rightVector.Length != matrix.Size)
+                throw new ArgumentException("Vectors and the matrix should be the same size.", nameof(rightVector));
+            if (resultVector.Length != matrix.Size)
+                throw new ArgumentException("Vectors and the matrix should be the same size.", nameof(resultVector));
+            
+            SolveCore(matrix, rightVector, resultVector);
+        }
+        
+        private static void SolveCore(NormalizedToeplitzMatrix L, float[] d, float[] s) {
+            
+        }
+    }
+}
