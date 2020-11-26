ec1566c4a473eea0b10ecb65b2a2593646bd2a48
p1x <p1xel.com@gmail.com>
2020-11-26 22:10:47 +0300

    Add test project. Add NormalizedToeplitzMatrix and tests.

---
+++ P1X.Toeplitz.Tests/NormalizedToeplitzMatrixTests.cs
@@ -0,0 +1,98 @@
+using System;
+using Xunit;
+
+namespace P1X.Toeplitz.Tests {
+    public class NormalizedToeplitzMatrixTests {
+        [Fact]
+        public void CreateWithSize_EmptyMatrix() {
+            var matrix = NormalizedToeplitzMatrix.Create(2);
+            
+            Assert.Equal(2, matrix.Size);
+        }
+
+        [Fact]
+        public void CreateWithZeroOrNegativeSize_ThrowsException() {
+            Assert.Throws<ArgumentOutOfRangeException>(() => NormalizedToeplitzMatrix.Create(0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => NormalizedToeplitzMatrix.Create(-2));
+        }
+
+        [Fact]
+        public void CreateWithValues_MatrixWithSameValues() {
+            var values = new float[] { 2, 3, 4, 5 };
+            
+            var matrix = NormalizedToeplitzMatrix.Create(values);
+            
+            Assert.Equal(3, matrix.Size);
+            Assert.Equal(1, matrix[0]);
+            Assert.Equal(values[0], matrix[-1]);
+            Assert.Equal(values[1], matrix[-2]);
+            Assert.Equal(values[2], matrix[1]);
+            Assert.Equal(values[3], matrix[2]);
+        }
+
+        [Fact]
+        public void CreateWithNoValues_MatrixWithOneElement() {
+            var matrix = NormalizedToeplitzMatrix.Create(new float[0]);
+            
+            Assert.Equal(1, matrix.Size);
+            Assert.Equal(1, matrix[0]);
+        }
+
+        [Fact]
+        public void CreateWithOddNumberOfValues_ThrowException() {
+            Assert.Throws<ArgumentException>(() => NormalizedToeplitzMatrix.Create(new float[3]));
+        }
+
+        [Fact]
+        public void Get_ReturnValidValue() {
+            var matrix = NormalizedToeplitzMatrix.Create(new float[] { 2, 3 });
+            
+            Assert.Equal(2, matrix[-1]);
+            Assert.Equal(3, matrix[1]);
+        }
+
+        [Fact]
+        public void GetOutOfRangeIndex_ThrowException() {
+            var matrix = NormalizedToeplitzMatrix.Create(2);
+            
+            Assert.Throws<IndexOutOfRangeException>(() => matrix[2]);
+            Assert.Throws<IndexOutOfRangeException>(() => matrix[-2]);
+        }
+        
+        [Fact]
+        public void Set_ChangeValue() {
+            var matrix = NormalizedToeplitzMatrix.Create(new float[] { 2, 3 });
+
+            matrix[-1] = 4;
+            matrix[1] = 5;
+            
+            Assert.Equal(4, matrix[-1]);
+            Assert.Equal(5, matrix[1]);
+        }
+
+        [Fact]
+        public void SetZeroIndex_ThrowException() {
+            var matrix = NormalizedToeplitzMatrix.Create(new float[] { 2, 3 });
+
+            Assert.Throws<IndexOutOfRangeException>(() => matrix[0] = 4);
+        }
+
+        [Fact]
+        public void SetOutOfRangeIndex_ThrowException() {
+            var matrix = NormalizedToeplitzMatrix.Create(new float[] { 2, 3 });
+            
+            Assert.Throws<IndexOutOfRangeException>(() => matrix[-2] = 4);
+            Assert.Throws<IndexOutOfRangeException>(() => matrix[2] = 5);
+        }
+
+        [Fact]
+        public void DefaultInstanceIsInitialized_ReturnFalse() {
+            Assert.False(default(NormalizedToeplitzMatrix).IsInitialized);
+        }
+
+        [Fact]
+        public void CreatedInstanceIsInitialized_ReturnTrue() {
+            Assert.True(NormalizedToeplitzMatrix.Create(2).IsInitialized);
+        }
+    }
+}
---
+++ P1X.Toeplitz/NormalizedToeplitzMatrix.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace P1X.Toeplitz {
+    public readonly struct NormalizedToeplitzMatrix {
+        private readonly float[] _values;
+
+        private NormalizedToeplitzMatrix(int size, float[] values) => (Size, _values) = (size, values);
+
+        public static NormalizedToeplitzMatrix Create(int size) {
+            if (size < 1)
+                throw new ArgumentOutOfRangeException(nameof(size), size, "Size should be grater then 1.");
+            
+            return new NormalizedToeplitzMatrix(size, new float[(size - 1) * 2]);
+        }
+
+        public static NormalizedToeplitzMatrix Create(float[] values) {
+            if (values.Length % 2 != 0)
+                throw new ArgumentException("Array length should be even.", nameof(values));
+            
+            var size = values.Length / 2 + 1;
+            return new NormalizedToeplitzMatrix(size, values);
+        }
+
+        public int Size { get; }
+
+        public float this[int index] {
+            get => Get(index);
+            set => Set(index, value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private float Get(int index) {
+            if (index >= Size || index <= -Size)
+                throw new IndexOutOfRangeException("The index should be between -N and N, where N is the size of the matrix.");
+            if (index == 0)
+                return 1;
+
+            return index < 0 
+                ? _values[-index - 1] 
+                : _values[index - 1 + (Size - 1)];
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void Set(int index, float value) {
+            if (index == 0)
+                throw new IndexOutOfRangeException("Can't set main diagonal values for the normalized matrix.");
+            if (index >= Size || index <= -Size)
+                throw new IndexOutOfRangeException("The index should be between -N and N, where N is the size of the matrix.");
+
+            if (index < 0)
+                _values[-index - 1] = value;
+            else
+                _values[index - 1 + (Size - 1)] = value;
+        }
+
+        public bool IsInitialized => _values != null;
+    }
+}
