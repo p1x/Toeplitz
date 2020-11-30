877ed564ca74c28225766f8cb81e3141e920f4c7
p1x <p1xel.com@gmail.com>
2020-12-01 01:02:06 +0300

    Update language version to 8.0 Solver refactoring.

--- P1X.Toeplitz/Solver.cs
+++ P1X.Toeplitz/Solver.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Threading;
 
 namespace P1X.Toeplitz {
     /// <summary>
     /// Main implementation of Zohar-Trench algorithm.
     /// https://doi.org/10.1145/321812.321822
     /// </summary>
+    [SuppressMessage("ReSharper", "SuggestBaseTypeForParameter")]
     public class Solver : ISolver {
         public void Solve(NormalizedToeplitzMatrix matrix, float[] rightVector, float[] resultVector) {
             if (!matrix.IsInitialized)
@@ -22,34 +26,23 @@ namespace P1X.Toeplitz {
             SolveCore(matrix, rightVector, resultVector);
         }
         
-        private void SolveCore(NormalizedToeplitzMatrix L, float[] d, float[] s) {
-            var e = new float[L.Size - 1]; // reversed vector
-            var g = new float[L.Size - 1];
+        private void SolveCore(NormalizedToeplitzMatrix matrix, float[] d, float[] s) {
+            var e = new float[matrix.Size - 1]; // reversed vector
+            var g = new float[matrix.Size - 1];
             
             s[0] = d[0];
             var lambda = 1f;
             
-            for (var i = 0; i < L.Size - 1; i++) {
-                CalculateHelpers(L, i, e, g, ref lambda);
-                CalculateIntermediateResult(L, i + 1, d, e, lambda, s);
+            for (var i = 0; i < matrix.Size - 1; i++) {
+                CalculateInversion(matrix, i, e, g, ref lambda);
+                CalculateIntermediateResult(matrix, i + 1, d, e, lambda, s);
             }
         }
 
-        private static void CalculateHelpers(NormalizedToeplitzMatrix L, int i, float[] e, float[] g, ref float lambda) {
-            var (eta, gamma) = CalculateHelperValues(L, i, e, g);
-
-            var etaOverLambda = eta / lambda;
-            var gammaOverLambda = gamma / lambda;
-
-            CalculateHelperVectors(i, e, g, etaOverLambda, gammaOverLambda);
-
-            lambda -= eta * gammaOverLambda;
-        }
-
-        private static void CalculateIntermediateResult(NormalizedToeplitzMatrix L, int i, float[] d, float[] e, float lambda, float[] s) {
+        private static void CalculateIntermediateResult(NormalizedToeplitzMatrix matrix, int i, float[] d, float[] e, float lambda, float[] s) {
             var theta = d[i];
             for (var j = 0; j < i; j++)
-                theta -= s[j] * L[i - j];
+                theta -= s[j] * matrix[i - j];
 
             var thetaOverLambda = theta / lambda;
             for (var j = 0; j < i; j++)
@@ -57,31 +50,47 @@ namespace P1X.Toeplitz {
             s[i] = thetaOverLambda;
         }
 
-        private static (float eta, float gamma) CalculateHelperValues(NormalizedToeplitzMatrix L, int i, float[] e, float[] g) {
-            var eta = -L[-(i + 1)];
-            var gamma = -L[i + 1];
+        private static void CalculateInversion(NormalizedToeplitzMatrix matrix, int i, float[] e, float[] g, ref float lambda) {
+            var eta = CalculateEta(matrix, i, e);
+            var gamma = CalculateGamma(matrix, i, g);
 
-            for (var j = 0; j < i; j++) {
-                eta -= L[-(j + 1)] * e[j];
-                gamma -= g[j] * L[i - j];
-            }
+            var etaOverLambda = eta / lambda;
+            var gammaOverLambda = gamma / lambda;
 
-            return (eta, gamma);
+            CalculateInversionVectors(i, e, g, etaOverLambda, gammaOverLambda);
+
+            lambda -= eta * gammaOverLambda;
         }
 
-        private static void CalculateHelperVectors(int i, float[] e, float[] g, float etaOverLambda, float gammaOverLambda) {
-            var ejPrev = e[0]; // e[j] from prev iteration
-            e[0] = etaOverLambda;
+        private static float CalculateEta(NormalizedToeplitzMatrix matrix, int i, float[] e) {
+            var eta = -matrix[-(i + 1)];
+            for (var j = 0; j < i; j++)
+                eta -= matrix[-(j + 1)] * e[j];
+            return eta;
+        }
+
+        private static float CalculateGamma(NormalizedToeplitzMatrix L, int i, float[] g) {
+            var gamma = -L[i + 1];
+            for (var j = 0; j < i; j++)
+                gamma -= g[j] * L[i - j];
+            return gamma;
+        }
+
+        private static void CalculateInversionVectors(int i, float[] e, float[] g, float etaOverLambda, float gammaOverLambda) {
+            var ejPrev = GetSet(ref e[0], etaOverLambda);
             for (var j = 0; j < i; j++) {
-                var gjPrev = g[j];
-
-                var ej1 = ejPrev + etaOverLambda * gjPrev;
-                g[j] = gjPrev + gammaOverLambda * ejPrev;
-
-                ejPrev = e[j + 1];
-                e[j + 1] = ej1;
+                var gjPrev = GetSet(ref g[j], g[j] + gammaOverLambda * ejPrev);
+                ejPrev = GetSet(ref e[j + 1], ejPrev + etaOverLambda * gjPrev);
             }
             g[i] = gammaOverLambda;
+        }
+
+        // Set new value and return original value
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static float GetSet(ref float target, in float value) {
+            var result = target;
+            target = value;
+            return result;
         }
     }
 }
