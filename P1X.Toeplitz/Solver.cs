e5c276ee190c6107eff19caee7ae272b5b397c94
p1x <p1xel.com@gmail.com>
2020-12-04 06:40:54 +0300

    Replace all calculations with simd. Some minor cleanup.

--- P1X.Toeplitz/Solver.cs
+++ P1X.Toeplitz/Solver.cs
@@ -19,30 +19,30 @@ namespace P1X.Toeplitz {
         private float[] _columnCache;
         private float[] _rowCache;
 
+        private int _vectorsLength;
+        
         public Solver(int maxMatrixSize) {
-            var count = System.Numerics.Vector<float>.Count;
-            var size = count * (int) Math.Ceiling((maxMatrixSize - 1) / (float) count);
-            _e = new float[size]; 
-            _g = new float[size];
-            _columnCache = new float[size + System.Numerics.Vector<float>.Count];
-            _rowCache = new float[size];
+            var vCount = System.Numerics.Vector<float>.Count;
+            _vectorsLength = vCount * (int) Math.Ceiling((maxMatrixSize - 1) / (float) vCount);
+            _e = new float[_vectorsLength + vCount]; 
+            _g = new float[_vectorsLength];
+            _columnCache = new float[_vectorsLength + vCount];
+            _rowCache = new float[_vectorsLength];
         }
 
         private void ResizeArrays() {
-            var vectorsLength = _e.Length;
-            if (_index < _e.Length)
-                return;
+            ResizeArray(ref _e, SN.Vector<float>.Count, 0);
+            ResizeArray(ref _g, 0, 0);
+            ResizeArray(ref _columnCache, SN.Vector<float>.Count, _vectorsLength);
+            ResizeArray(ref _rowCache, 0, 0);
 
-            void Resize(ref float[] array, int additionalSpace, int copyOffset) {
-                var newArray = new float[vectorsLength * 2 + additionalSpace];
-                array.CopyTo(newArray, copyOffset);
-                array = newArray;
-            }
-            
-            Resize(ref _e, 0, 0);
-            Resize(ref _g, 0, 0);
-            Resize(ref _columnCache, SN.Vector<float>.Count, vectorsLength);
-            Resize(ref _rowCache, 0, 0);
+            _vectorsLength *= 2;
+        }
+
+        private void ResizeArray(ref float[] array, int additionalSpace, int copyOffset) {
+            var newArray = new float[_vectorsLength * 2 + additionalSpace];
+            array.CopyTo(newArray, copyOffset);
+            array = newArray;
         }
 
         public void Solve(NormalizedToeplitzMatrix matrix, Vector rightVector, Vector resultVector) {
@@ -57,19 +57,15 @@ namespace P1X.Toeplitz {
         }
 
         private void SolveUnchecked(NormalizedToeplitzMatrixUnchecked matrix, Vector d, Vector s) {
-            var count = System.Numerics.Vector<float>.Count;
+            var vCount = System.Numerics.Vector<float>.Count;
             
-            var size2 = count * (int) Math.Ceiling(s.Values.Length / (float) count);
-
-            var s2 = s;
-            if (s.Values.Length != size2) 
-                s2 = new Vector(new float[size2]);
-
+            var size = vCount * (int) Math.Ceiling(s.Values.Length / (float) vCount);
+            var s2 = s.Values.Length == size ? s : new Vector(new float[size]);
+            
             s2[0] = d[0];
-
             while (_index < matrix.Size - 1) 
                 IterateUnchecked(matrix, d, s2);
-
+            
             if (s2.Values != s.Values) 
                 Array.Copy(s2.Values, s.Values, s.Values.Length);
         }
@@ -86,13 +82,14 @@ namespace P1X.Toeplitz {
         }
 
         private void IterateUnchecked(NormalizedToeplitzMatrixUnchecked matrix, Vector rightVector, Vector resultVector) {
-            ResizeArrays();
+            if (_index >= _vectorsLength)
+                ResizeArrays();
 
             if (_index == 0) 
                 CalculateIntermediateResult(_index, rightVector, resultVector);
 
             _columnCache[GetColumnCacheIndex(_index + 1)] = matrix[_index + 1];
-            _rowCache[0 + _index] = matrix[-(_index + 1)];
+            _rowCache[_index] = matrix[-(_index + 1)];
             
             CalculateInversion(_index);
             CalculateIntermediateResult(_index + 1, rightVector, resultVector);
@@ -111,12 +108,12 @@ namespace P1X.Toeplitz {
             for (var j = 0; j < i; j+= SN.Vector<float>.Count) {
                 var sv = new SN.Vector<float>(sValues, j);
                 var cv = new SN.Vector<float>(_columnCache, iColumnCache + j);
-                var sum = SN.Vector.Dot(sv * cv, SN.Vector<float>.One);
+                var sum = SN.Vector.Dot(sv, cv);
                 
                 theta -= sum;
             }
 
-            var thetaOverLambda = theta / _lambda;
+            var thetaOverLambda = new SN.Vector<float>(theta / _lambda);
 
             for (var j = 0; j < i; j+= SN.Vector<float>.Count) {
                 var ev = new SN.Vector<float>(_e, j);
@@ -125,7 +122,7 @@ namespace P1X.Toeplitz {
                 var r = sv + ev * thetaOverLambda;
                 r.CopyTo(sValues, j);
             }
-            s[i] = thetaOverLambda;
+            s[i] = thetaOverLambda[0];
         }
 
         private void CalculateInversion(int i) {
@@ -142,7 +139,7 @@ namespace P1X.Toeplitz {
             for (var j = 0; j < i; j += SN.Vector<float>.Count) {
                 var ev = new SN.Vector<float>(_e, j);
                 var rv = new SN.Vector<float>(_rowCache, j);
-                var sum = SN.Vector.Dot(ev * rv, SN.Vector<float>.One);
+                var sum = SN.Vector.Dot(ev, rv);
 
                 eta -= sum;
             }
@@ -157,7 +154,7 @@ namespace P1X.Toeplitz {
             for (var j = 0; j < i; j+= SN.Vector<float>.Count) {
                 var gv = new SN.Vector<float>(_g, j);
                 var cv = new SN.Vector<float>(_columnCache, iColumnCache + j);
-                var sum = SN.Vector.Dot(gv * cv, SN.Vector<float>.One);
+                var sum = SN.Vector.Dot(gv, cv);
 
                 gamma -= sum;
             }
@@ -166,23 +163,24 @@ namespace P1X.Toeplitz {
         }
 
         private void CalculateInversionVectors(int i, float eta, float gamma) {
-            var etaOverLambda = eta / _lambda;
-            var gammaOverLambda = gamma / _lambda;
-            
-            var ejPrev = GetSet(ref _e[0], etaOverLambda);
-            for (var j = 0; j < i; j++) {
-                var gjPrev = GetSet(ref _g[j], _g[j] + gammaOverLambda * ejPrev);
-                ejPrev = GetSet(ref _e[j + 1], ejPrev + etaOverLambda * gjPrev);
-            }
-            _g[i] = gammaOverLambda;
-        }
+            var etaOverLambda = new SN.Vector<float>(eta / _lambda);
+            var gammaOverLambda = new SN.Vector<float>(gamma / _lambda);
 
-        // Set new value and return original value
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float GetSet(ref float target, in float value) {
-            var result = target;
-            target = value;
-            return result;
+            var ev = new SN.Vector<float>(_e, 0); 
+            for (var j = 0; j < i; j += SN.Vector<float>.Count) {
+                var gv = new SN.Vector<float>(_g, j);
+
+                var gvNew = gv + gammaOverLambda * ev;
+                var evNew = ev + etaOverLambda * gv;
+
+                ev = new SN.Vector<float>(_e, j + SN.Vector<float>.Count);
+
+                gvNew.CopyTo(_g, j);
+                evNew.CopyTo(_e, j + 1);
+            }
+            
+            _e[0] = etaOverLambda[0];
+            _g[i] = gammaOverLambda[0];
         }
 
         void ISolver<NormalizedToeplitzMatrix, Vector, Vector>.Solve(NormalizedToeplitzMatrix matrix, Vector rightVector, Vector resultVector) => 
